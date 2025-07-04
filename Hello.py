3107ca3c7e1d4f5d0a650f88b67e9b99f161ea70
Diego Salcedo <diego.salcedo.70.360@gmail.com>
2025-07-04 17:23:51 -0500

    add README file

--- Hello.py
+++ Hello.py
@@ -1,3 +1,7 @@
-print("Hello, World!")
+print("Hello, World!!!")
 # This script prints "Hello, World!" to the console.
 # It serves as a simple demonstration of a Python script.
+New_var = 0
+for i in range(10):
+    New_var += i
+    
